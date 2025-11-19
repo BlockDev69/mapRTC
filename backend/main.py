1134b500c1f63b3c8e608bd020ab9a48e668019f
BlockDev <bisimwad6@gmail.com>
2025-11-19 16:43:47 +0100

    Change allow origins

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
+    allow_origins=["http://127.0.0.1:3000",
                    "http://localhost:3000",
     ],
     allow_credentials=True,
